af38c0eb9dbeea35f958cd9579e032f2fad779f1
geniustrade <164870394+geniustrade@users.noreply.github.com>
2024-03-27 12:18:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+document.getElementById('logoutBtn').addEventListener('click', function() {
+    // Clear user session (remove token, etc.)
+    // For simplicity, let's redirect back to the login page
+    window.location.href = 'login.html';
+  });
+  
